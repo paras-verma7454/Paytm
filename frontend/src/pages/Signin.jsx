acca17487ea0311906394e161e487c0bb842bd6f
Paras Verma <134628559+paras-verma7454@users.noreply.github.com>
2024-09-10 11:39:52 +0530

    Update Signin.jsx

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -14,7 +14,7 @@ export const Signin = () => {
 
   const handleSignin = async () => {
     try {
-      const response = await axios.post("https://paytm-ym3c.onrender.com/api/v1/user/signin", {
+      const response = await axios.post("http://localhost:3000/api/v1/user/signin", {
         username,
         password
       });
