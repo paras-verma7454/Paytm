5dea9f92d53b15203af40076ce9799f39eead102
Paras Verma <134628559+paras-verma7454@users.noreply.github.com>
2024-09-10 14:58:20 +0530

    Update SendMoney.jsx

--- frontend/src/pages/SendMoney.jsx
+++ frontend/src/pages/SendMoney.jsx
@@ -42,7 +42,7 @@ export const SendMoney = () => {
                     />
                     </div>
                     <button onClick={() => {
-                        axios.post("http://localhost:3000/api/v1/account/transfer", {
+                        axios.post("https://paytm-ym3c.onrender.com/api/v1/account/transfer", {
                             to: id,
                             amount
                         }, {
