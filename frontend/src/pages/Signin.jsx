79be66d4dea049e9dddff5da9c2580923faac62b
Paras Verma <134628559+paras-verma7454@users.noreply.github.com>
2024-09-10 14:56:55 +0530

    Update Signin.jsx

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -34,7 +34,7 @@ export const Signin = () => {
           />
           <div className="pt-4">
           <Button onClick={async () => {
-            const response = await axios.post("http://localhost:3000/api/v1/user/signin", {
+            const response = await axios.post("https://paytm-ym3c.onrender.com/api/v1/user/signin", {
               username,
               password
             });
