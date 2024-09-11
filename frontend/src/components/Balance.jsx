12199d1a2afecf13370913b697cc99c2af299996
paras-verma7454 <parasverma7454@gmail.com>
2024-09-11 13:25:04 +0530

    Balance update

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -1,10 +1,21 @@
-export const Balance = ({ value }) => {
+import { useState,useEffect } from "react";
+import axios from "axios";
+export const Balance = () => {
+    const [balance, setBalance] = useState(null);
+    useEffect(()=>{
+         axios.get("https://paytm-ym3c.onrender.com/api/v1/account/balance", {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            }
+        }).then(response =>{setBalance(response.data.balance.toFixed(2));})
+        
+    })
     return <div className="flex">
         <div className="font-bold text-lg">
             Your balance
         </div>
         <div className="font-semibold ml-4 text-lg">
-            Rs {value}
+            Rs {balance}
         </div>
     </div>
 }
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -6,7 +6,7 @@ export const Dashboard = () => {
     return <div>
         <Appbar />
         <div className="m-8">
-            <Balance value={"10,000"} />
+            <Balance />
             <Users />
         </div>
     </div>
